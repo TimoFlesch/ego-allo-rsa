0ffc15909571d9f752d4cb04aebe327b83f3bbc1
Timo Flesch <24828674+TimoFlesch@users.noreply.github.com>
2022-08-26 16:02:42 +0100

    added support for yaml config files

--- ego_allo_rnns/utils/config.py
+++ ego_allo_rnns/utils/config.py
@@ -1,8 +1,37 @@
+import torch
+import yaml
+
 from ego_allo_rnns.configs.rnn import cfg
 
 
-def load_config(cfg_id: str = "default") -> dict:
+def load_config(cfg_id: str = "default", cfg_path: str = "./configs/") -> dict:
     if cfg_id == "default":
         return cfg
-    else:  # todo
-        return cfg
+    else:
+        # load corresponding yaml file and convert to dict
+        with open(cfg_path + cfg_id + ".yml") as f:
+            config = yaml.safe_load(f)
+            config = dict(config)
+
+        # update fields
+        config = get_callable_lossfunct(config)
+        # return dict
+        return config
+
+
+def get_callable_lossfunct(cfg: dict) -> dict:
+    """slightly ugly helper function that replaces str in dict created from yaml file
+       with corresponding callable. E.g. if "mse" provided, replace with torch.nn.MSELoss()
+
+    Args:
+        cfg (dict): config dict
+
+    Returns:
+        dict: updated config dict
+    """
+
+    if cfg["training"]["criterion"].upper() == "MSE":
+        cfg["training"]["criterion"] = torch.nn.MSELoss()
+    elif cfg["training"]["criterion"].upper() == "MAE":
+        cfg["training"]["criterion"] = torch.nn.L1Loss()
+    return cfg
