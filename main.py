ca766a4c4bd714ea4fb90701aee4fcde9f33133d
Timo Flesch <24828674+TimoFlesch@users.noreply.github.com>
2022-08-31 11:13:01 +0100

    first batch of experiment results

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from ego_allo_rnns.configs.rnn import cfg_SCSC, cfg_SCWC, cfg_WCSC, cfg_WCWC
 from ego_allo_rnns.trainers.train_rnn import run_training
+from ego_allo_rnns.utils.config import load_config
 from ego_allo_rnns.utils.io import save_training_results
 
 if __name__ == "__main__":
 
-    configs = [cfg_WCWC, cfg_WCSC, cfg_SCWC, cfg_SCSC]
-    for cfg in configs:
+    configs = ["WCWC", "WCSC", "SCWC", "SCSC"]
+    for cfg_id in configs:
+        try:
+            cfg = load_config(cfg_id=cfg_id, cfg_path="./configs/experiments/")
+        except FileNotFoundError:
+            cfg = load_config(cfg_id=cfg_id.lower(), cfg_path="./configs/experiments/")
+
         model, results = run_training(cfg)
         save_training_results(cfg, model, results)
